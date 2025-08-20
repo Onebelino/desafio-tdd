5148e5050f428afa669c96225787a53ff7338737
João Vitor <umbelino3005@gmail.com>
2025-08-20 18:13:23 -0300

    01 & 02 - Prontos

--- venda.js
+++ venda.js
@@ -1,6 +1,29 @@
-
-
 function calcularVenda(venda) {
-  // Implementação da função
+  let subtotal = 0;
+
+  venda.itens.forEach(item => {
+    let valorComDesconto = item.valorItem;
+
+    //DESAFIO 01 - DESCONTO PERCENTUAL
+    if (item.descontoPercentual > 0) {
+      valorComDesconto -= (item.valorItem * item.descontoPercentual) / 100;
+    }
+
+    //DESAFIO 02 - DESCONTO REAL
+    if (item.descontoReal > 0) {
+      valorComDesconto -= item.descontoReal;
+    }
+
+    if(item.descontoPercentual > item.descontoReal) {
+
+    }
+
+    subtotal += valorComDesconto;
+  });
+
+  return {
+    totalFinal: subtotal
+  };
 }
+
 module.exports = calcularVenda;
